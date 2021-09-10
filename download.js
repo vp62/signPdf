179e949cbdbeef41b2e1cc856cf1767b69a5c994
vp62 <pcfor62@gmail.com>
2021-09-10 15:48:37 +0530

    i think std o/p with delete option

--- download.js
+++ download.js
@@ -11,20 +11,22 @@ async function embedImages(pdfData) {
     console.log(pdfUrl)
     const pdfDoc = await PDFDocument.load(fs.readFileSync(pdfUrl));
     const pages = pdfDoc.getPages()
-    const firstPage = pages[0]
     imgdet.forEach(async (img_data) => {
+        const writePage = pages[img_data.page]
+        console.log(img_data.page)
         // console.log(img_data)
         const pngImage = await pdfDoc.embedPng(img_data.img)
         // // // Add a blank page to the document
         console.log(img_data.x, img_data.y, img_data.width, img_data.height)
-        console.log(firstPage.getWidth(), firstPage.getHeight())
-        console.log(firstPage.getX(), firstPage.getY())
-        console.log(firstPage.getPosition(), firstPage.getArtBox())
-        console.log((firstPage.getHeight() - 792) - img_data.height)
+        console.log(writePage.getWidth(), writePage.getHeight())
+        console.log(writePage.getX(), writePage.getY())
+        console.log(writePage.getPosition(), writePage.getArtBox())
+        console.log((writePage.getHeight() -img_data.y) - img_data.height/2)
         // // // Draw the PNG image near the lower right corner of the JPG image
-        firstPage.drawImage(pngImage, {
+        writePage.drawImage(pngImage, {
             x: img_data.x,
-            y: firstPage.getHeight() - img_data.y - img_data.height / 2 - 123.54,
+
+            y: writePage.getHeight() -img_data.y-img_data.height/2-126.434,
             width: img_data.width,
             height: img_data.height,
         })
--- public/javascripts/signService.js
+++ public/javascripts/signService.js
@@ -45,38 +45,50 @@ var translatePos = {
   x: 0,
   y: 0
 };
+
+let which_page=1;
+document.querySelector('.container').addEventListener('scroll',()=>{
+  // console.log(document.querySelector('.container').scrollTop)
+  which_page=Math.ceil((document.querySelector('.container').scrollTop/641))
+  console.log(which_page)
+
+})
 let sign, signHolder
 document.getElementById('apply').addEventListener('click', (event) => {
-  // var sign=document.createElement('')
-
   sign = document.createElement('img');
   signHolder = document.createElement('div');
   data = signaturePad.toDataURL('image/png');
   sign.src = data;
-  sign.setAttribute('class', 'sign')
+  sign.setAttribute('class', 'sign');
   var tright = document.createElement('div')
   var tleft = document.createElement('div')
   var bleft = document.createElement('div')
   var bright = document.createElement('div')
+  var dicon = document.createElement('i')
+  dicon.setAttribute('class',"fa fa-trash");
   tright.setAttribute('class', 'tright');
   tleft.setAttribute('class', 'tleft');
   bright.setAttribute('class', 'bright');
   bleft.setAttribute('class', 'bleft');
   signHolder.appendChild(tright)
+  tright.appendChild(dicon)
   signHolder.appendChild(tleft)
   signHolder.appendChild(bleft)
   signHolder.appendChild(bright)
-  // signHolder.setAttribute('class','draggable-sign');
-  let page = document.getElementById('holder-page-1');
+  console.log(`page is${which_page}`+"page is")
+
+  let page = document.getElementById(`holder-page-${which_page||1}`);
   page.classList.add('dragarea');
   signHolder.classList.add('draggable');
+ 
   sign.onload = () => {
     console.log('image ready')
     signHolder.appendChild(sign)
+   
   }
-
+    
   page.append(signHolder)
-
+deletebtn()
   download_btn.style.display = "none";
   save_btn.style.display = "block";
   const position = { x: 0, y: 0 }
@@ -84,10 +96,11 @@ document.getElementById('apply').addEventListener('click', (event) => {
 })
 
 save_btn.addEventListener('click', () => {
-  document.getElementById('signbutton').style.display = 'none';
-  save_btn.style.display = 'none';
+  // document.getElementById('signbutton').style.display = 'none';
+  // save_btn.style.display = 'none';
   download_btn.style.display = 'block';
 })
+
 let width = 20, height = 10;
 const position = { x: 0, y: 0 }
 interact('.draggable')
@@ -109,6 +122,7 @@ interact('.draggable')
         })
         console.log(x, y, position.x, position.y)
         Object.assign(event.target.dataset, { x, y })
+        // saveit()
       }
     }
   })
@@ -124,15 +138,27 @@ interact('.draggable')
 
         event.target.style.transform =
           `translate(${position.x}px, ${position.y}px)`
+          // saveit()''
+          console.log(event.target.style.transform)
       },
     }
   })
 
 var imgdet = [];
 function saveit() {
-  imgdet.push({ img: data.toString(), x: position.x || 306, y: position.y || 396, width: width, height: height })
-  console.log('saved')
-  console.log(JSON.stringify(imgdet))
+ var is_there= imgdet.findIndex((img_data,index)=>{
+  console.log(typeof(img_data.img)) 
+  console.log(typeof(data.toString())) 
+  if(img_data.img.toString() == data.toString()){
+    return index;
+  }})
+ console.log(is_there)
+ if(is_there==-1){
+
+   imgdet.push({ img: data.toString(), x: position.x || 306, y: position.y || 396, width: width, height: height ,page:which_page-1})
+   console.log('saved')
+   console.log(JSON.stringify(imgdet))
+  }
 }
 function downloadit() {
   console.log(sign)
@@ -150,3 +176,39 @@ function downloadit() {
     .then(function (res) { console.log(res) })
     .catch(function (res) { console.log(res) })
 }
+function deletebtn(){
+  console.log('loaded');
+  var icon=document.querySelectorAll('.fa-trash');
+  icon.forEach(item=>item.addEventListener('click',(e)=>{
+   
+    var is_there= imgdet.findIndex((img_data)=>{
+     return img_data.img == item.parentNode.parentNode.querySelector('.sign').getAttribute('src')});
+    //  {
+    //     return index;
+    //   }})
+     console.log(is_there)
+     if(is_there!=-1){
+     
+       imgdet.splice(is_there,1)
+       item.parentNode.parentNode.remove()
+       console.log('deleted')
+       console.log(JSON.stringify(imgdet))
+      }
+      else{
+       item.parentNode.parentNode.remove()
+
+      }
+  }));
+
+}
+setInterval(()=>{
+  console.log(document.querySelectorAll('.draggable')[0]+" check")
+  let sign_Exists=document.querySelectorAll('.draggable');
+  if(sign_Exists!=undefined){
+     sign_Exists.forEach(sign_Wrapper=>sign_Wrapper.addEventListener('click',()=>{
+      sign_Wrapper.classList.toggle('active');
+      console.log(sign_Wrapper.querySelector('.sign').getAttribute('src'))
+      console.log('clicked')
+    }))//EOF sign container check
+  }
+},3000)
